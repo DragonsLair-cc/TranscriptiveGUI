b267676fc8c1586e99285a4fba54da4614dc9a26
ZoeTheDragon-Git <github@dragonslair.cc>
2026-03-02 11:31:05 -0500

    Created the field of medicine dropdown menu and grabbed the selection

--- ViewModels/TranscriptiveViewModel.cs
+++ ViewModels/TranscriptiveViewModel.cs
@@ -1,4 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 
 namespace LinuxGUI.ViewModels;
 
@@ -6,5 +8,15 @@ public partial class TranscriptiveViewModel : ViewModelBase
 {
     // This is the code for the {Binding SampleName} - it just generates the public property
     [ObservableProperty]
-    private string? _sampleName; 
+    private string? _sampleName;
+
+    public ObservableCollection<string> Options { get; } = new() { "Cardiology", "Neurology", "General Medicine" };
+
+    [ObservableProperty]
+    private string? _mediField;
+
+    public void saveField()
+    {
+        System.Diagnostics.Debug.WriteLine($"Saved: {MediField}");
+    }
 }

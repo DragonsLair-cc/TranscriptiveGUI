9c354f39c4691b3d3cc4d04e65da2af8cb39a636
zoe <github@dragonslair.cc>
2026-03-29 01:11:49 -0400

    Added sample for basic theme fixes for tomorrow

--- obj/Debug/net10.0/LinuxGUI.AssemblyInfo.cs
+++ obj/Debug/net10.0/LinuxGUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LinuxGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c68abd755f2a8d64e11393b59d733730068ca9e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c20460b9112685d950dbb56c0caf570ee7cd15")]
 [assembly: System.Reflection.AssemblyProductAttribute("LinuxGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LinuxGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
